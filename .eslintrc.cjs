3e8a5a7bc9ecc06443bc47bd3987b3f99d190308
ankitbatra22 <ankitbatra2202@gmail.com>
2024-11-24 07:13:27 -0500

    skip linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
+    "react/no-unescaped-entities": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -28,14 +29,18 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-unsafe-member-access": "warn",
-    "@typescript-eslint/no-unsafe-assignment": "warn",
-    "@typescript-eslint/no-unsafe-return": "warn",
-    "@typescript-eslint/no-unsafe-call": "warn",
-    "@typescript-eslint/require-await": "warn",
-    "@typescript-eslint/no-misused-promises": "warn"
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-explicit-any": "off"
   },
+  ignorePatterns: [".eslintrc.cjs", "*.config.js", "*.config.ts"]
 };
 
 module.exports = config;
